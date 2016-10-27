53ef9cff11d7f9f3e5db5bdfd4999da230f18949
Parrikar, Amit <Amit.Parrikar@cdk.com>
2016-10-27 15:44:00 +0530

    Created empty services

---
+++ client/src/app/services/twitter.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TwitterService {
+
+  constructor() { }
+
+  getTwitterData(count: number, screenName: string){
+
+    return []; //return twitter data
+
+  }
+
+}
