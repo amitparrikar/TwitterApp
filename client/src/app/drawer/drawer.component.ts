e23b24db942b93ff930d10fe35328ea6e99d8db0
Parrikar, Amit <Amit.Parrikar@cdk.com>
2016-10-27 15:08:03 +0530

    Header, Body, drawer empty components created

---
+++ client/src/app/drawer/drawer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[twt-drawer]',
+  templateUrl: './drawer.component.html',
+  styleUrls: ['./drawer.component.css']
+})
+export class DrawerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
